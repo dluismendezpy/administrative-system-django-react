36c804ed4873ec718082e6b6733febc6359ea33d
Luis Mendez <dluismendezdj@gmail.com>
2022-04-08 14:34:41 -0400

    Create admin class for models

--- administrative_system/services/admin.py
+++ administrative_system/services/admin.py
@@ -1,3 +1,55 @@
+# Django
 from django.contrib import admin
 
-# Register your models here.
+# Owns
+from .models import Service, Board
+
+
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    """Admin for Service model"""
+    fields = (
+        "name",
+        "is_active",
+        "created_at",
+    )
+    list_display = (
+        "name",
+        "is_active",
+        "created_at",
+    )
+    list_display_links = (
+        "name",
+    )
+    readonly_fields = (
+        "created_at",
+    )
+    ordering = (
+        "-created_at",
+    )
+
+
+@admin.register(Board)
+class BoardAdmin(admin.ModelAdmin):
+    """Admin for Board model"""
+    fields = (
+        "name",
+        "user",
+        "service",
+        "is_active",
+        "created_at",
+    )
+    list_display = (
+        "name",
+        "is_active",
+        "created_at",
+    )
+    list_display_links = (
+        "name",
+    )
+    readonly_fields = (
+        "created_at",
+    )
+    ordering = (
+        "-created_at",
+    )
