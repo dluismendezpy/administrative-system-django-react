f646df7d10922473cc390a7ceebca29da264d1b1
Luis Mendez <dluismendezdj@gmail.com>
2022-04-08 10:44:08 -0400

    Register models

--- administrative_system/users/admin.py
+++ administrative_system/users/admin.py
@@ -1,3 +1,57 @@
+# Ownss
 from django.contrib import admin
 
-# Register your models here.
+# Owns
+from .models import User, City
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    """class for User admin"""
+    fields = (
+        "first_name",
+        "last_name",
+        "email",
+        "is_active",
+        "is_latino",
+        "gender",
+    )
+    list_display = (
+        "first_name",
+        "last_name",
+        "email",
+        "is_active",
+        "is_latino",
+        "gender",
+    )
+    list_display_links = (
+        "first_name",
+    )
+    readonly_fields = (
+        "created_at",
+    )
+    ordering = (
+        "-created_at",
+    )
+
+
+@admin.register(City)
+class CityAdmin(admin.ModelAdmin):
+    """class for City model"""
+    fields = (
+        "name",
+        "created_at",
+    )
+    list_display = (
+        "name",
+        "created_at",
+    )
+    list_display_links = (
+        "name",
+    )
+    readonly_fields = (
+        "created_at",
+    )
+    ordering = (
+        "-created_at",
+    )
