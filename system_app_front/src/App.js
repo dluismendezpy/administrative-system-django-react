973f6caaecc33d7bae83eb95e46cb4a9c7e588be
Enmanuel Mendez <katariinaop@gmail.com>
2022-04-09 17:57:01 -0400

    commit 9/4, 5 56 pm

--- system_app_front/src/App.js
+++ system_app_front/src/App.js
@@ -1,25 +1,20 @@
 import logo from "./logo.svg";
 import "./App.css";
 
-function App() {
+import { Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Navigation from "./pages/Navigation";
+import NoMatch from "./pages/NoMatch";
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  
+    <Routes>
+      <Route path="home" element={<Home />} />
+      <Route path="navigation" element={<Navigation />} />
+      <Route path="*" element={<NoMatch />} />
+    </Routes>
+
   );
 }
-
-export default App;
--- system_app_front/src/pages/NoMatch.jsx
+++ system_app_front/src/pages/NoMatch.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-export default class Navigation extends React.Component {
+export default class NoMatch extends React.Component {
   render() {
     return (
       <div>
-        <h1>Navigation page</h1>
+        <h1>404 page</h1>
       </div>
     );
   }
