8ab2d28fd430fb64a30e8df95023b707b75e2d5b
Luis Mendez <dluismendezdj@gmail.com>
2022-04-11 09:58:06 -0400

    Add auth token to create user

--- administrative_system/administrative_system/urls.py
+++ administrative_system/administrative_system/urls.py
@@ -2,10 +2,14 @@
 from django.contrib import admin
 from django.urls import path, include
 
+# Rest Framework
+from rest_framework.authtoken.views import obtain_auth_token
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("users/", include("users.urls")),
     path("services/", include("services.urls")),
+    path("auth/", obtain_auth_token)
 ]
 
 admin.site.site_header = "MS Mendez Services"
