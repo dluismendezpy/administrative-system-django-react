e70b623d7d4f8b708ceec1c2b76ded2f29b3fe51
Luis Mendez <dluismendezdj@gmail.com>
2022-05-16 15:13:30 -0400

    Clear msjs

--- system_app_front/src/pages/Signup/Signup.jsx
+++ system_app_front/src/pages/Signup/Signup.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import axios from "axios";
 import "./Signup.css";
 import Navigation from "../../components/Navigation/Navigation";
 import Footer from "../../components/Footer/Footer";
--- system_app_front/src/pages/SobreNosotros/SobreNosotros.jsx
+++ system_app_front/src/pages/SobreNosotros/SobreNosotros.jsx
@@ -2,10 +2,6 @@ import React from "react";
 import "./SobreNosotros.css";
 import Navigation from "../../components/Navigation/Navigation";
 import Footer from "../../components/Footer/Footer";
-import { Card } from "react-bootstrap";
-import img1 from "../../assets/images/card1.png";
-import img2 from "../../assets/images/card2.jpg";
-import img3 from "../../assets/images/card3.jpg";
 
 export default class Home extends React.Component {
   render() {
